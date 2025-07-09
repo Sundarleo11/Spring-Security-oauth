a93e91c02fdf73bbcfc593388a2466e991926d96
Sundarleo11 <sundarrock1999@gmail.com>
2025-07-09 12:53:35 +0530

    spring boot setup

---
+++ springsecurity/src/test/java/com/easybytes/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankBackendApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
