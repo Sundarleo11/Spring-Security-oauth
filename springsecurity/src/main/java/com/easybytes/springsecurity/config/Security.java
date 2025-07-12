51834b4e67137d0d504adc7d1450c8da8c651d86
Sundarleo11 <sundarrock1999@gamil.com>
2025-07-12 20:16:21 +0530

    passwordEncoder

--- springsecurity/src/main/java/com/easybytes/springsecurity/config/Security.java
+++ springsecurity/src/main/java/com/easybytes/springsecurity/config/Security.java
@@ -3,6 +3,12 @@ package com.easybytes.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,8 +27,22 @@ public class Security {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
-
-
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$gKOLfwU8LGIUZFuNCDVe3OH07RCd2hfAZyWX/tQucsmnrkq7d5bNe")//12345
+                .authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }
